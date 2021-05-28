738a1eb4a2fd9df24f168549669a8ace09ac6938
Eko Junaidi Salam <eko_junaidisalam@live.com>
2021-05-28 14:01:36 +0700

    Add Test case

---
+++ tests/HelperTest.php
@@ -0,0 +1,33 @@
+<?php declare(strict_types=1);
+
+namespace EJSHelper\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class HelperTest extends TestCase {
+
+    /**
+     * @group base64
+     */
+    public function testBase64UrlEncode(): void
+    {
+        $this->assertEquals("aHR0cHM6Ly93d3cuYmFzZTY0dXJsLmNvbQ",base64url_encode("https://www.base64url.com"));
+    }
+
+    /**
+     * @group base64
+     */
+    public function testBase64UrlDecode(): void
+    {
+        $this->assertEquals("https://www.base64url.com",base64url_decode("aHR0cHM6Ly93d3cuYmFzZTY0dXJsLmNvbQ"));
+    }
+
+    /**
+     * @group codechal
+     */
+    public function testCodeChallenge(): void
+    {
+        list($code,$chal) = codeChallenge("dBjftJeZ4CVP-mB92K27uhbUJU1p1r_wW1gFWFOEjXk");
+        $this->assertEquals("E9Melhoa2OwvFrEMTJguCHaoeK1t8URWbuGJSstw-cM",$chal);
+    }
+}
